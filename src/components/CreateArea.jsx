e70e70da45bad41482059a1cadd02972be0ee895
khorGu <bekaxorgu31@gmail.com>
2022-05-12 12:16:55 +0400

    Deployng

---
+++ src/components/CreateArea.jsx
@@ -0,0 +1,67 @@
+import { useState } from "react";
+import AddIcon from "@mui/icons-material/Add";
+import Fab from "@mui/material/Fab";
+import Zoom from "@mui/material/Zoom";
+const CreateArea = (props) => {
+  const [isExpanded, setExpandend] = useState(false);
+
+  const [note, setNote] = useState({
+    title: "",
+    content: "",
+  });
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+
+    setNote((prevNote) => {
+      return {
+        ...prevNote,
+        [name]: value,
+      };
+    });
+  };
+
+  const submitNote = (event) => {
+    props.onAdd(note);
+    setNote({
+      title: "",
+      content: "",
+    });
+    event.preventDefault();
+  };
+
+  function expand() {
+    setExpandend(true);
+  }
+
+  return (
+    <div>
+      <form className="create-note">
+        {isExpanded && (
+          <input
+            onChange={handleChange}
+            name="title"
+            value={note.title}
+            placeholder="Title"
+          />
+        )}
+
+        <textarea
+          onChange={handleChange}
+          name="content"
+          onClick={expand}
+          value={note.content}
+          placeholder="Take a note..."
+          rows={isExpanded ? 3 : 1}
+        />
+        <Zoom in={isExpanded}>
+          <Fab onClick={submitNote}>
+            <AddIcon />
+          </Fab>
+        </Zoom>
+      </form>
+    </div>
+  );
+};
+
+export default CreateArea;
---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+const Footer = () => {
+
+    let year = new Date().getFullYear();
+    return (  
+        
+        <footer>
+            <p>Copyright © {year}</p>
+        </footer>
+    );
+}
+ 
+export default Footer;
